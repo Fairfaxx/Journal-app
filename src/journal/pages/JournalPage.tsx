1c0bcc44823728330f04ce84137c194a8b8dfd45
FefeLemaire <federico.lemaire@alloy.digital>
2022-11-23 13:09:38 -0300

    floating button added to create new entries

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,5 +1,6 @@
-import { MailOutline } from '@mui/icons-material';
-import { Typography } from '@mui/material';
+import { AddOutlined, MailOutline } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material';
+import { positions } from '@mui/system';
 import React from 'react';
 import JournalLayout from '../layout/JournalLayout';
 import NoteView from '../views/NoteView';
@@ -8,8 +9,22 @@ import NothingSelectedView from '../views/NothingSelectedView';
 const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.5 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
