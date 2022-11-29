bf7c79cbeaf14766835ccb678136a669eb216b3e
Fede <53632624+Fairfaxx@users.noreply.github.com>
2022-11-29 16:04:36 -0300

    Feature/firestore setup (#13)
    * firestore setup and new notes are being added
    
    * added new functionality to display the screen depending of the state, disabled button to add new new while is creating a note
    
    Co-authored-by: FefeLemaire <federico.lemaire@alloy.digital>

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,34 @@
+import { doc, setDoc } from 'firebase/firestore/lite';
+import { FirebaseDB } from '../../firebase/config';
+import { collection } from 'firebase/firestore/lite';
+import { addNewEmptyNote, savingNewNote, setActiveNote } from './journalSlice';
+
+export const startNewNote = () => {
+  return async (dispatch, getState) => {
+    dispatch(savingNewNote());
+
+    const { uid } = getState().auth;
+
+    //uid
+
+    const newNote = {
+      title: '',
+      body: '',
+      date: new Date().getTime(),
+    };
+
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+    await setDoc(newDoc, newNote);
+
+    console.log({ newDoc, setDoc });
+
+    newNote.id = newDoc.id;
+
+    dispatch(addNewEmptyNote(newNote));
+    dispatch(setActiveNote(newNote));
+
+    // dispatch
+    //dispatch(newNote)
+    //dispatch(activarNote)
+  };
+};
