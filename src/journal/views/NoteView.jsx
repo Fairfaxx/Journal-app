3d34d8beab6b0ea37bf8ea5eccdb6aa43804ebfe
FefeLemaire <federico.lemaire@alloy.digital>
2022-12-01 16:14:38 -0300

    sweet alert added and working

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -2,19 +2,41 @@ import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import React, { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
 import { useForm } from '../../hooks/useForm';
 import { setActiveNote } from '../../store/journal/journalSlice';
 import { startSaveNote } from '../../store/journal/thunks';
 import ImageGallery from '../components/ImageGallery';
 
 const NoteView = () => {
-  const { active: note } = useSelector((state) => state.journal);
+  const {
+    active: note,
+    messageSaved,
+    isSaving,
+  } = useSelector((state) => state.journal);
   const dispatch = useDispatch();
   const { title, body, date, onInputChange, formState } = useForm(note);
 
   useEffect(() => {
     dispatch(setActiveNote(formState));
   }, [formState]);
+
+  useEffect(() => {
+    if (messageSaved.length > 0) {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+
+    return () => {
+      Swal.close();
+    };
+  }, [messageSaved]);
 
   const dateString = useMemo(() => {
     const newDate = new Date(date);
@@ -40,7 +62,12 @@ const NoteView = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <Button onClick={onSaveNote} color="primary" sx={{ padding: 2 }}>
+        <Button
+          disabled={!!isSaving}
+          onClick={onSaveNote}
+          color="primary"
+          sx={{ padding: 2 }}
+        >
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
           Save
         </Button>
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -25,13 +25,14 @@ export const journalSlice = createSlice({
     },
     setActiveNote: (state, action) => {
       state.active = action.payload;
+      state.messageSaved = '';
     },
     setNotes: (state, action) => {
       state.notes = action.payload;
     },
     setSaving: (state) => {
       state.isSaving = true;
-      //TODO: handle error msg
+      state.messageSaved = '';
     },
     updateNote: (state, action) => {
       state.isSaving = false;
@@ -41,6 +42,7 @@ export const journalSlice = createSlice({
         }
         return note;
       });
+      state.messageSaved = `${action.payload.title}, was updated successfully`;
     },
     deleteNoteById: (state, action) => {},
   },
